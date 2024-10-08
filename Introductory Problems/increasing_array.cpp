6912c032c6d76bd1984b305a9ee107e458041e79
FLopriore <115494520+FLopriore@users.noreply.github.com>
2024-10-08 17:22:47 +0200

    Increasing Array

--- Introductory Problems/increasing_array.cpp
+++ Introductory Problems/increasing_array.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <vector>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    int n; std::cin >> n;
+
+    long long int prev; std::cin >> prev;
+    long long int move = 0;
+
+    for (int i=1; i<n; ++i) {
+        long long int x; std::cin >> x;
+        if (x<prev) {
+            move += prev-x;
+            x = prev;
+        }
+        prev = x;
+    }
+
+    std::cout << move << std::endl;
+}
